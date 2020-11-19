7a77ab2ce42884e730fe292d5f58c22e09a3e4c3
Hosam ALAZA <Hosam ALAZA@DESKTOP-M3EL2QG>
2020-11-19 02:38:41 +0200

    this version new

--- github/Program.cs
+++ github/Program.cs
@@ -11,6 +11,7 @@ namespace github
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            Console.WriteLine("hello in githup");
         }
     }
 }
